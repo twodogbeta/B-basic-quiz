50e520687d1a0e7f5e9a3cc0c6415e863dbd73e4
twodogbeta <qijinhaoup@163.com>
2020-09-15 12:06:17 +0800

    [qjh] Initialize project

---
+++ src/main/java/com/thoughtworks/capability/gtb/basicquiz/GtbBasicQuizApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.capability.gtb.basicquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtbBasicQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtbBasicQuizApplication.class, args);
+	}
+
+}
